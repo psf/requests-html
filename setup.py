53c7409d7c7241ac0cf2158f09e14cf5f1245bcc
Kenneth Reitz <me@kennethreitz.org>
2018-02-24 16:19:59 -0500

    setup.py
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -33,8 +33,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+# with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+#     long_description = '\n' + f.read()
+long_description = ''
 
 class UploadCommand(Command):
     """Support setup.py upload."""
