a00dfda754b5cd8f3c86f4a5ede69147905d1ef8
Rohit Gupta <guptarohit@users.noreply.github.com>
2018-02-26 21:52:42 +0530

    remove python 3.4 classifier
    Since it require python >= 3.5.0

--- setup.py
+++ setup.py
@@ -93,7 +93,6 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
