2b22c2f55d4feef0e35dd9cbf04a54a27e42c884
Kenneth Reitz <me@kennethreitz.org>
2018-02-28 07:50:59 -0500

    3.6 and up
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    python_requires='>=3.5.0',
+    python_requires='>=3.6.0',
     # If your package is a single module, use this instead of 'packages':
     py_modules=['requests_html'],
 
@@ -93,7 +93,6 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
