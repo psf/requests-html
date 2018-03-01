6370ed4a20e53c2fee156289eb9bc2aafbf412b4
Kenneth Reitz <me@kennethreitz.org>
2018-03-01 08:06:58 -0500

    commit
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- requests_html.py
+++ requests_html.py
@@ -18,6 +18,7 @@ from w3lib.encoding import html_to_unicode
 
 DEFAULT_ENCODING = 'utf-8'
 DEFAULT_URL = 'https://example.org/'
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8'
 
 useragent = UserAgent()
 
