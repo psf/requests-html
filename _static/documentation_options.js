20de64c584449c862a1e6d4889ee655ffdafb4ae
Kenneth Reitz <me@kennethreitz.org>
2018-03-03 10:16:48 -0500

    fix
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'v0.3.4',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
