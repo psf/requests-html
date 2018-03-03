df1b037f743a7b55b32357e9456cd0cda2c13912
Kenneth Reitz <me@kennethreitz.org>
2018-03-03 09:53:00 -0500

    v0.8.0
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DESCRIPTION = 'HTML Parsing for Humans.'
 URL = 'https://github.com/kennethreitz/requests-html'
 EMAIL = 'me@kennethreitz.org'
 AUTHOR = 'Kenneth Reitz'
-VERSION = '0.7.4'
+VERSION = '0.8.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
