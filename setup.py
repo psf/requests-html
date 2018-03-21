30b59cc25cf32dcb08751911ec0c0cc17d0c5dde
Kenneth Reitz <me@kennethreitz.org>
2018-03-21 07:33:31 -0400

    pin 0.0.14
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ VERSION = '0.8.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'requests', 'pyquery', 'fake-useragent', 'parse', 'bs4', 'w3lib', 'pyppeteer'
+    'requests', 'pyquery', 'fake-useragent', 'parse', 'bs4', 'w3lib', 'pyppeteer>=0.0.14'
 ]
 
 # The rest you shouldn't have to touch too much :)
