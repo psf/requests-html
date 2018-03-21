a25b3737adc93c8f72a7b2c77a93a4ec49ae06dd
Ordanis Sanchez <ordanisanchez@gmail.com>
2018-03-21 15:39:56 -0400

    Add async iterator to HTML class

--- tests/test_internet.py
+++ tests/test_internet.py
@@ -1,6 +1,8 @@
-from requests_html import HTMLSession
+import pytest
+from requests_html import HTMLSession, AsyncHTMLSession
 
 session = HTMLSession()
+
 
 def test_pagination():
     pages = (
@@ -14,3 +16,17 @@ def test_pagination():
         r = session.get(page)
         assert next(r.html)
 
+
+@pytest.mark.asyncio
+async def test_pagination(event_loop):
+    asession = AsyncHTMLSession()
+    pages = (
+        'https://xkcd.com/1957/',
+        'https://reddit.com/',
+        'https://smile.amazon.com/',
+        'https://theverge.com/archives'
+    )
+
+    for page in pages:
+        r = await asession.get(page)
+        assert await r.html.__anext__()
