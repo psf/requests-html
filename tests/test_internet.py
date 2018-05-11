599be5ddb010ad7e040d775fa8158fb3ac00ed57
Pedro Rodrigues <medecau@gmail.com>
2018-05-11 16:47:18 +0200

    adds pagination test

--- tests/test_internet.py
+++ tests/test_internet.py
@@ -1,4 +1,7 @@
-from requests_html import HTMLSession
+import os
+from urllib import parse
+
+from requests_html import HTMLSession, HTML
 
 session = HTMLSession()
 
@@ -13,4 +16,13 @@ def test_pagination():
     for page in pages:
         r = session.get(page)
         assert next(r.html)
+
+
+def test_real_pagination():
+    url = 'https://news.ycombinator.com/'
+    sample_path = path = os.path.sep.join((os.path.dirname(os.path.abspath(__file__)), 'hn-sample.html'))
+    html = open(sample_path).read()
+    page = HTML(html=html, url=url)
+    next_url = page._next()
+    assert parse.urlparse(url).netloc == parse.urlparse(next_url).netloc
 
