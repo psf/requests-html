a42fb29df39eba76e267cbe468005bbc73546a1c
Asugawara <asgasw@gmail.com>
2023-05-03 09:16:44 +0900

    apply black

--- setup.py
+++ setup.py
@@ -12,16 +12,22 @@ from shutil import rmtree
 from setuptools import setup, Command
 
 # Package meta-data.
-NAME = 'requests-html'
-DESCRIPTION = 'HTML Parsing for Humans.'
-URL = 'https://github.com/psf/requests-html'
-EMAIL = 'me@kennethreitz.org'
-AUTHOR = 'Kenneth Reitz'
-VERSION = '0.10.0'
+NAME = "requests-html"
+DESCRIPTION = "HTML Parsing for Humans."
+URL = "https://github.com/psf/requests-html"
+EMAIL = "me@kennethreitz.org"
+AUTHOR = "Kenneth Reitz"
+VERSION = "0.10.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'requests', 'pyquery', 'fake-useragent', 'parse', 'beautifulsoup4', 'w3lib', 'pyppeteer>=0.0.14'
+    "requests",
+    "pyquery",
+    "fake-useragent",
+    "parse",
+    "beautifulsoup4",
+    "w3lib",
+    "pyppeteer>=0.0.14",
 ]
 
 # The rest you shouldn't have to touch too much :)
@@ -33,19 +39,20 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -55,20 +62,20 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
-        self.status('Publishing git tags…')
-        os.system('git tag v{0}'.format(VERSION))
-        os.system('git push --tags')
+        self.status("Publishing git tags…")
+        os.system("git tag v{0}".format(VERSION))
+        os.system("git push --tags")
 
         sys.exit()
 
@@ -82,27 +89,26 @@ setup(
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    python_requires='>=3.6.0',
+    python_requires=">=3.6.0",
     # If your package is a single module, use this instead of 'packages':
-    py_modules=['requests_html'],
-
+    py_modules=["requests_html"],
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
     # },
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     # $ setup.py publish support.
     cmdclass={
-        'upload': UploadCommand,
+        "upload": UploadCommand,
     },
 )
